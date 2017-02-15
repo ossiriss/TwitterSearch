36422574b43e05a5c3205fe51d87694d4575a724
Boris Estrin <borisestrin@gmail.com>
2017-02-15 12:33:49 +0200

    everything works

--- Example/TwitterSearch/ViewController.swift
+++ Example/TwitterSearch/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import TwitterSearch
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        TwitterSearch.Api.getTweetsArray(searchPhrase: "#barcelona #psg"){status,tweetsArray,error in
+            if status{
+                for tweet in tweetsArray!{
+                    print("\(tweet)\n")
+                }
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ TwitterSearch/Classes/Api.swift
@@ -0,0 +1,33 @@
+//
+//  Api.swift
+//  Pods
+//
+//  Created by Admin on 15/02/2017.
+//
+//
+
+import Foundation
+import SwiftyJSON
+import Alamofire
+
+public class Api{
+    public static func getTweetsArray(searchPhrase: String, completion: @escaping ((Bool, [String]?, String?)->Void)){
+        
+        var urlString = "https://api.twitter.com/1.1/search/tweets.json?q=" + searchPhrase + "&result_type=recent"
+        var url = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        Alamofire.request(url!, headers: ["Authorization":"Bearer AAAAAAAAAAAAAAAAAAAAABjczAAAAAAAmpsyum03hAwA3jfPdbcpIrWLwXY%3Dg5wRYqAQUdGvCiPYoWV6vAsJ5ELWctM37PDkaAFXeX1NOFgn8Y"])
+            .responseJSON { (response) in
+                if response.result.isSuccess{
+                    let json = JSON(response.result.value)
+                    let tweetsArray = json["statuses"].arrayValue
+                    var result = [String]()
+                    for tweet in tweetsArray{
+                        result.append(tweet["text"].stringValue)
+                    }
+                    completion(true, result, nil)
+                } else{
+                    completion(false, nil, "error")
+                }
+        }
+    }
+}
